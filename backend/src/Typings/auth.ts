4ede6300c01ef4ba441a4c815121af4da5cec579
Gowtham118 <saigowtham@gmail.com>
2023-12-09 15:32:32 +0530

    fix: typings

---
+++ backend/src/Typings/auth.ts
@@ -0,0 +1,13 @@
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsPhoneNumber,
+  IsString,
+} from 'class-validator';
+
+export class UserDTO {
+  id: number;
+  CreatedAt: string;
+  UpdatedAt: string;
+  // TODO
+}
